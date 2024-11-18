8a675d3d525b6cc52c140fd87682c726c88847d0
Felix Mayer <FelixM@yer.tools>
2024-11-18 01:09:52 +0100

    Update schema

--- backend/priv/migrations/1730062059_initial_schema_migration.sql
+++ backend/priv/migrations/1730062059_initial_schema_migration.sql
@@ -2,14 +2,143 @@
 -- gleam run -m feather -- new "Initial schema migration"
 -- gleam run -m feather -- schema
 
-CREATE TABLE "page_state" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"previous_state"	INTEGER,
-	"time"	INTEGER NOT NULL,
-	"name"	TEXT,
-	"json"	BLOB NOT NULL,
-	"html"	BLOB,
-	"css"	BLOB,
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("previous_state") REFERENCES "page_state"("id")
+---------
+-- TABLES
+---------
+
+-- This table contains configuration information.
+-- It is used to store general settings from this instance.
+CREATE TABLE "configuration" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"key" TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"value" TEXT NOT NULL,
+	"created_at" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
+
+-- This table contains user information including their credentials.
+-- Those users are maintainer or editor of the website.
+CREATE TABLE "user" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"email"	TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"password"	BLOB NOT NULL,
+	"salt"	BLOB NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- A session is used for the storage of a user's authentication information.
+-- It is used to identify the user and to grant access to specific parts of the website.
+-- Sessions can be deleted on logout or after they expire.
+CREATE TABLE "session" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"key" BLOB NOT NULL UNIQUE,
+	"user_id"	INTEGER NOT NULL,
+	"created_at"	INTEGER NOT NULL,
+	"expires_at"	INTEGER NOT NULL,
+	"user_agent"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("user_id") REFERENCES "user"("id")
+);
+
+-- A visitor is used to trace navigation's from everyone on the website.
+-- It is used to group requests.
+CREATE TABLE "visitor" (
+	"id" INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- A visitor session is used to link a session to a visitor.
+-- Sessions are generated on first request and are stored as a cookie.
+-- Since crawlers or automated scripts could flood this table, entries are deleted after some time.
+CREATE TABLE "visitor_session" (
+	"session_key" BLOB NOT NULL UNIQUE,
+	"visitor_id" INTEGER NOT NULL UNIQUE,
+	"created_at" INTEGER NOT NULL,
+	PRIMARY KEY("session_key"),
+	FOREIGN KEY("visitor_id") REFERENCES "visitor"("id")
+);
+
+-- This represents a path that a user visited.
+-- It is used to avoid string duplication.
+CREATE TABLE "request_path" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"path" TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- A page request is used to track page views.
+-- Every request is logged here and can be used to generate statistics.
+-- Since those requests are linked to a visitor, they can show navigation patterns.
+CREATE TABLE "page_request" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"visitor_id" INTEGER NOT NULL,
+	"request_path_id" INTEGER NOT NULL,
+	"time" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("visitor_id") REFERENCES "visitor"("id"),
+	FOREIGN KEY("request_path_id") REFERENCES "request_path"("id")
+);
+
+-- This represents a page with its content.
+CREATE TABLE "page" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"path" TEXT NOT NULL COLLATE NOCASE,
+
+	"title" TEXT NOT NULL,
+	"description" TEXT,
+	
+	"widgets_json" BLOB NOT NULL,
+	"generated_html" BLOB,
+	"generated_css" BLOB,
+	
+	"created_at" INTEGER NOT NULL,
+	"updated_at" INTEGER NOT NULL,
+
+	"active" INTEGER NOT NULL,
+
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- This represents a file with its content.
+-- Files have a hash to avoid duplication.
+CREATE TABLE "file" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"hash" BLOB NOT NULL UNIQUE,
+	"data" BLOB NOT NULL,
+	"size" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- This represents a file's metadata.
+-- The key is used for security reasons to avoid searching for ids.
+CREATE TABLE "file_metadata" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"file_id" INTEGER NOT NULL,
+	"key" TEXT NOT NULL,
+	"name" TEXT NOT NULL,
+	"content_type" TEXT NOT NULL,
+	"created_at" INTEGER NOT NULL,
+	"deleted" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("file_id") REFERENCES "file"("id")
+);
+
+----------
+-- INDEXES
+----------
+
+CREATE INDEX "configuration_key_created_at" ON "configuration" ("key", "created_at" DESC);
+
+CREATE INDEX "user_username" ON "user" ("username");
+CREATE INDEX "user_email" ON "user" ("email");
+
+CREATE INDEX "session_key" ON "session" ("key");
+
+CREATE INDEX "request_path_path" ON "request_path" ("path");
+
+CREATE INDEX "page_request_visitor_id" ON "page_request" ("visitor_id");
+
+CREATE INDEX "page_active_path_updated_at" ON "page" ("active", "path", "updated_at" DESC);
+
+
--- backend/schema.sql
+++ backend/schema.sql
@@ -1,16 +1,114 @@
 CREATE TABLE storch_migrations (id integer, applied integer);
 
-CREATE TABLE "page_state" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"previous_state"	INTEGER,
-	"time"	INTEGER NOT NULL,
-	"name"	TEXT,
-	"json"	BLOB NOT NULL,
-	"html"	BLOB,
-	"css"	BLOB,
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("previous_state") REFERENCES "page_state"("id")
+CREATE TABLE "configuration" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"key" TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"value" TEXT NOT NULL,
+	"created_at" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE sqlite_sequence(name,seq);
+
+CREATE TABLE "user" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"email"	TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"password"	BLOB NOT NULL,
+	"salt"	BLOB NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "session" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"key" BLOB NOT NULL UNIQUE,
+	"user_id"	INTEGER NOT NULL,
+	"created_at"	INTEGER NOT NULL,
+	"expires_at"	INTEGER NOT NULL,
+	"user_agent"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("user_id") REFERENCES "user"("id")
+);
+
+CREATE TABLE "visitor" (
+	"id" INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "visitor_session" (
+	"session_key" BLOB NOT NULL UNIQUE,
+	"visitor_id" INTEGER NOT NULL UNIQUE,
+	"created_at" INTEGER NOT NULL,
+	PRIMARY KEY("session_key"),
+	FOREIGN KEY("visitor_id") REFERENCES "visitor"("id")
+);
+
+CREATE TABLE "request_path" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"path" TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "page_request" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"visitor_id" INTEGER NOT NULL,
+	"request_path_id" INTEGER NOT NULL,
+	"time" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("visitor_id") REFERENCES "visitor"("id"),
+	FOREIGN KEY("request_path_id") REFERENCES "request_path"("id")
+);
+
+CREATE TABLE "page" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"path" TEXT NOT NULL COLLATE NOCASE,
+
+	"title" TEXT NOT NULL,
+	"description" TEXT,
+	
+	"widgets_json" BLOB NOT NULL,
+	"generated_html" BLOB,
+	"generated_css" BLOB,
+	
+	"created_at" INTEGER NOT NULL,
+	"updated_at" INTEGER NOT NULL,
+
+	"active" INTEGER NOT NULL,
+
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "file" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"hash" BLOB NOT NULL UNIQUE,
+	"data" BLOB NOT NULL,
+	"size" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "file_metadata" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"file_id" INTEGER NOT NULL,
+	"key" TEXT NOT NULL,
+	"name" TEXT NOT NULL,
+	"content_type" TEXT NOT NULL,
+	"created_at" INTEGER NOT NULL,
+	"deleted" INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("file_id") REFERENCES "file"("id")
+);
+
+CREATE INDEX "configuration_key_created_at" ON "configuration" ("key", "created_at" DESC);
+
+CREATE INDEX "user_username" ON "user" ("username");
+
+CREATE INDEX "user_email" ON "user" ("email");
+
+CREATE INDEX "session_key" ON "session" ("key");
+
+CREATE INDEX "request_path_path" ON "request_path" ("path");
+
+CREATE INDEX "page_request_visitor_id" ON "page_request" ("visitor_id");
+
+CREATE INDEX "page_active_path_updated_at" ON "page" ("active", "path", "updated_at" DESC);
 
